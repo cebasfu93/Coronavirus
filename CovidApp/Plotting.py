722b37b7451af0c3d1559ddd36be0d138da7d8d6
Sebastian Franco-Ulloa <cebasfu93@gmail.com>
2020-08-02 16:16:09 +0200

    Update Plotting.py

--- CovidApp/Plotting.py
+++ CovidApp/Plotting.py
@@ -3,22 +3,20 @@ from Constants import *
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
 def from_rgb(rgb):
-    """translates an rgb tuple of int to a tkinter friendly color code
-    """
     rgb = tuple(int(c*255) for c in rgb)
     return "#%02x%02x%02x" % rgb
 
-def plot_metrics(gui, data, date_ini='2020-01-01'):
-    pop = data.loc[data.population.notna(),'population'].loc[yday]
-    dates = pd.date_range(date_ini, yday, periods=10)
-    dates_mini = pd.date_range(date_ini, yday, periods=19)
+def plot_metrics(gui, data, date_ini='2020-01-01', date_fin=yday):
+    pop = data.loc[data.population.notna(),'population'].loc[date_fin]
+    dates = pd.date_range(date_ini, date_fin, periods=10)
+    dates_mini = pd.date_range(date_ini, date_fin, periods=19)
     dateticks = [t.strftime('%b-%d') for t in dates]
     data_keep = data.loc[data.index>date_ini]
 
     new_cases_warn = int(NCI_max*pop/(100000*DT_int))
 
-    fig, axs = plt.subplots(figsize=(6,6), ncols=1, nrows=3, sharex=True)
-    plots_tk = FigureCanvasTkAgg(fig, gui)
+    fig, axs = plt.subplots(figsize=(6,7.5), ncols=1, nrows=3, sharex=True)
+    plots_tk = FigureCanvasTkAgg(fig, master=gui)
     plots_tk.get_tk_widget().grid(column=2, columnspan=5, row=1, rowspan=15)#, sticky="WENS")
 
     for ax in axs:
@@ -32,35 +30,34 @@ def plot_metrics(gui, data, date_ini='2020-01-01'):
 
     #axs[0] -> New cases
     axs[0].set_ylabel('New cases', fontsize=Z)
-    axs[0].set_ylim((0, data_keep.new_cases.max()+100))
-    axs[0].fill_between(data_keep.index, 0, data_keep.new_cases.rolling(DT_int, center=True).mean(), alpha=0.15, color=(0,0,0.7))
-    axs[0].errorbar(data_keep.index, data_keep.new_cases.rolling(DT_int, center=True).mean(), lw=3.5, c=(0,0,0.7), label="2-week\nmoving average")
-    axs[0].errorbar(data_keep.index, data_keep.new_cases, alpha=0.3, c=(0.1,0.1,0.9), fmt='o-', markevery=1, mec='k', mew=1.2, lw=1, label='Data records')
-    axs[0].legend(fontsize=Z-4, shadow=True, fancybox=True, labelspacing=0.3, handlelength=1, handletextpad=0.6, borderpad=0.4)
+    axs[0].set_ylim((0, max(new_cases_warn+100, data_keep.new_cases.max()+100)))
+    axs[0].fill_between(data_keep.index, 0, data_keep.new_cases.rolling(DT_int, center=True).mean(), alpha=0.15, color=(0,0,0.7), zorder=1)
+    axs[0].errorbar(data_keep.index, data_keep.new_cases.rolling(DT_int, center=True).mean(), lw=3.5, c=(0,0,0.7), label="2-week\nmoving average", zorder=2)
+    axs[0].errorbar(data_keep.index, data_keep.new_cases, alpha=0.3, c=(0.1,0.1,0.9), fmt='o-', markevery=1, mec='k', mew=1.2, lw=1, label='Data records', zorder=3)
+    l = axs[0].legend(fontsize=Z-2, shadow=True, fancybox=True, labelspacing=0.3, handlelength=1, handletextpad=0.6, borderpad=0.4)
+    l.set_zorder(10)
+
     axs[0].grid()
     ax1 = axs[0].twinx()
-    ax1.set_ylabel("Necessary daily cases\n-for two weeks-\nfor 'high incidence'", rotation=270, fontsize=Z-3, backgroundcolor=(0.9,0.9,0.9), labelpad=50)
-    ax1.axhline(new_cases_warn, lw=1.5, c=(0.2,0.2,0.2), ls='--')
-    ax1.set_ylim((0, data_keep.new_cases.max()+100))
+    ax1.set_ylabel("Necessary daily cases\n-for two weeks-\nfor 'high incidence'", rotation=270, fontsize=Z-3, backgroundcolor=(0.9,0.9,0.9), labelpad=40)
+    ax1.axhline(new_cases_warn, lw=1.5, c=(0.2,0.2,0.2), ls='--', zorder=4)
+    ax1.set_ylim((0, max(new_cases_warn+100, data_keep.new_cases.max()+100)))
     ax1.set_yticks([new_cases_warn])
     ax1.tick_params(labelsize=Z, labelcolor=(0.2,0.2,0.2))
-    #plt.savefig("New_cases.png", format='png', dpi=300)
-    #plt.show()
+
 
     #axs[1] -> NCI
     axs[1].set_ylabel('NCI', fontsize=Z)
     axs[1].set_ylim((0, max(25, data_keep.NCI.max()+10)))
     axs[1].fill_between(data_keep.index, 0, data_keep.NCI, alpha=0.15, color=(1,0,0.7))
-    axs[1].errorbar(data_keep.index, data_keep.NCI, lw=3.5, c=(0.7,0,0.4))#, label="2-week\nmoving average")
+    axs[1].errorbar(data_keep.index, data_keep.NCI, lw=3.5, c=(0.7,0,0.4))
     axs[1].grid()
     ax1 = axs[1].twinx()
     ax1.axhline(20, lw=1.5, c=(0.2,0.2,0.2), ls='--')
     ax1.set_ylim((0, max(25, data_keep.NCI.max()+10)))
     ax1.set_yticks([20])
     ax1.tick_params(labelsize=Z, labelcolor=(0.2,0.2,0.2))
-    ax1.set_ylabel("Necessary NCI\nfor 'high incidence'", rotation=270, fontsize=Z-3, backgroundcolor=(0.9,0.9,0.9), labelpad=50)
-    #plt.savefig("NCI.png", format='png', dpi=300)
-    #plt.show()
+    ax1.set_ylabel("Necessary NCI\nfor 'high incidence'", rotation=270, fontsize=Z-3, backgroundcolor=(0.9,0.9,0.9), labelpad=40)
 
     #axs[2] -> PPT
     axs[2].set_xlabel('Date', fontsize=Z)
@@ -72,10 +69,8 @@ def plot_metrics(gui, data, date_ini='2020-01-01'):
 
     ax1 = axs[2].twinx()
     ax1.axhline(5, lw=1.5, c=(0.2,0.2,0.2), ls='--')
-    ax1.set_ylim((0, max(5, data_keep.PPT.max()+3)))
+    ax1.set_ylim((0, max(8, data_keep.PPT.max()+3)))
     ax1.set_yticks([5])
     ax1.tick_params(labelsize=Z, labelcolor=(0.2,0.2,0.2))
-    ax1.set_ylabel("Necessary PPT\nfor 'high incidence'", rotation=270, fontsize=Z-3, backgroundcolor=(0.9,0.9,0.9), labelpad=50)
+    ax1.set_ylabel("Necessary PPT\nfor 'high incidence'", rotation=270, fontsize=Z-3, backgroundcolor=(0.9,0.9,0.9), labelpad=40)
     plt.tight_layout()
-    #plt.savefig("PPT.png", format='png', dpi=300)
-    #plt.show()
