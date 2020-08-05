09f77a59b57f5bdaf2e012fd47f4a841e252510a
Sebastian Franco-Ulloa <cebasfu93@gmail.com>
2020-08-05 15:16:32 +0200

    Added begin_day as Jan1, 2020

--- CovidApp/Constants.py
+++ CovidApp/Constants.py
@@ -1,6 +1,8 @@
 import pandas as pd
 from datetime import timedelta
+from datetime import datetime
 
+cols = ['date', 'new_cases_per_million', 'new_cases', 'new_tests', 'population', 'continent', 'location']
 bg='white'
 
 NCI_max = 20
@@ -8,6 +10,9 @@ PPT_max = 5
 DT_int = 14
 today = pd.to_datetime('today').date()
 yday = today - timedelta(days=1)
+begin_day = datetime.strptime('2020-01-01', '%Y-%m-%d').date()
+dates = pd.date_range(begin_day, yday, freq='1D')
+dates_dict = {i : fecha for i, fecha in enumerate(dates)}
 DT = timedelta(days=DT_int)
 
 Z = 12
