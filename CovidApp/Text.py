979a8e8225e9526e4037b388950f7e5baa861fa3
Sebastian Franco-Ulloa <cebasfu93@gmail.com>
2020-08-05 15:17:16 +0200

    Displays NCI and PPT at separate dates

--- CovidApp/Text.py
+++ CovidApp/Text.py
@@ -5,18 +5,17 @@ The total number of new cases per 100,000 inhabitants in the last 14 days (NCI)\
 The percentage of positive tests per week averaged for the last 2 weeks (PPT)\n\n \
 In order for the Norwegian state to consider a country as\na high-incidence (i.e., high-risk) area, the country must have:\n"
 
-info_txt2 = "NCI  > {:d}\n\
-PPT >  {:d}\n\n".format(NCI_max, PPT_max)
+info_txt2 = "NCI  > {:d}\nPPT >  {:d}\n\n".format(NCI_max, PPT_max)
 
 info_txt3 = "-------------------------------------------------------------------------------------------------------------------\n"
 
 question_txt = 'Which country would you like to assess?'
 
-country_default = "Italy"
+country_default = "Select"
 
-latest_date_txt = "\nLatest date available: {}"
+latest_data_txt = "\nLatest data available:"
 
-metrics_txt = "NCI: {:.1f}\nPPT: {:.2f}\n"
+metrics_txt = "NCI: {:.2f}   ({})\nPPT: {:.2f}   ({})\n"
 
 date_min_txt = 'Minimum date to plot'
 date_max_txt = 'Maximum date to plot'
